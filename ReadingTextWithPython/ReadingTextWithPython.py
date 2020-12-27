f9188da46bab3e665f691c3bdc9b1135a68f0397
Hridaya Shrestha <shresthahridaya123@gmail.com>
2020-12-27 16:03:32 -0500

    first git thing

---
+++ ReadingTextWithPython/ReadingTextWithPython.py
@@ -0,0 +1,11 @@
+import os
+
+os.path.join(path, path1)
+path = os.path.join("\Users\hrida\OneDrive\Desktop", "ReadingTextWithPython.py")
+print(path)
+    
+#wikiText = open("kiwiWiki.txt", "r")
+#print(wikiText.read())
+#wikiText.close()
+
+#C:\Users\hrida\OneDrive\Desktop
