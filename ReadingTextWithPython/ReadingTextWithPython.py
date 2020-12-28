65c4de5da668e2ae6304f7490ed1ae5f29fcd593
Hridaya Shrestha <shresthahridaya123@gmail.com>
2020-12-28 17:20:13 -0500

    added path for wikipedia text file

--- ReadingTextWithPython/ReadingTextWithPython.py
+++ ReadingTextWithPython/ReadingTextWithPython.py
@@ -1,9 +1,12 @@
 import os
 
-os.path.join(path, path1)
+path = "C:\Users\hrida\OneDrive\Desktop\Learning Coding\PythonProjects\ReadingTextWithPython\Output"
 
 wikiText = open("kiwiWiki.txt", "r")
 print(wikiText.read())
 wikiText.close()
 
 #C:\Users\hrida\OneDrive\Desktop
+#C:\Users\hrida\OneDrive\Desktop\Learning Coding\PythonProjects\ReadingTextWithPython
+#C:\Users\hrida\OneDrive\Desktop\Learning Coding\PythonProjects\ReadingTextWithPython\Output+
+ 
